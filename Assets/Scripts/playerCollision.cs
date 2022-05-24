987813155fc32d09923818576055f75dd682b6ec
inakiurizar <101410794+inakiurizar@users.noreply.github.com>
2022-05-24 16:01:57 -0300

    COLLISION

---
+++ Assets/Scripts/playerCollision.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCollision : MonoBehaviour
+{
+    bool salto = false;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "DeathWall")
+        {
+            transform.position = new Vector3(3.47f, 0.5f, 3.45f);
+            transform.eulerAngles = new Vector3(0,0,0);
+        }
+        if (salto == true)
+        {
+            salto = false;
+        }
+    }
+
+    void OnCollisionExit()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            salto = true;
+        }
+        if (salto == false)
+        {
+            transform.position = new Vector3(3.47f, 0.5f, 3.45f);
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float movementSpeed;
+    public float rotationSpeed;
+    public float jump;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0,0, movementSpeed);
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -movementSpeed);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, rotationSpeed, 0);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -rotationSpeed, 0);
+        } 
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(0, jump, 0);
+        }
+        
+    }
+}
